7ac39ac8c0339a0c9d31f64b2180c048dad596a3
SAVIN P <49348281+savinpadencherry@users.noreply.github.com>
2026-06-18 02:37:34 +0530

    fix: render main dashboard directly

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,8 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
-
-import 'core/blocs/auth/auth_bloc.dart';
-import 'core/blocs/capture/capture_bloc.dart';
-import 'core/constants.dart';
-import 'core/get.dart';
-import 'core/repository/auth_repository.dart';
-import 'core/repository/capture_repository.dart';
-import 'core/services/navigator_service.dart';
 import 'theme/theme.dart';
-import 'views/splash/splash_view.dart';
+import 'views/main_shell/main_shell_view.dart';
 
 void main() {
-  WidgetsFlutterBinding.ensureInitialized();
-  initializeGetIt();
   runApp(const MainApplication());
 }
 
@@ -22,36 +11,11 @@ class MainApplication extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MultiRepositoryProvider(
-      providers: <RepositoryProvider<dynamic>>[
-        RepositoryProvider<AuthRepository>(
-          create: (_) => AuthRepository(),
-        ),
-        RepositoryProvider<CaptureRepository>(
-          create: (_) => CaptureRepository(),
-        ),
-      ],
-      child: MultiBlocProvider(
-        providers: <BlocProvider<dynamic>>[
-          BlocProvider<AuthBloc>(
-            create: (BuildContext context) => AuthBloc(
-              authRepository: context.read<AuthRepository>(),
-            )..add(const AuthStarted()),
-          ),
-          BlocProvider<CaptureBloc>(
-            create: (BuildContext context) => CaptureBloc(
-              captureRepository: context.read<CaptureRepository>(),
-            )..add(const CaptureRequested()),
-          ),
-        ],
-        child: MaterialApp(
-          title: ConstantVars.appName,
-          debugShowCheckedModeBanner: false,
-          navigatorKey: app<NavigatorService>().navigatorKey,
-          theme: AppTheme.light,
-          home: const SplashView(),
-        ),
-      ),
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Mr. Rex',
+      theme: AppTheme.light,
+      home: const MainShellView(),
     );
   }
 }
