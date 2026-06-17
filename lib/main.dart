3640de9a810ccbdfdcf5ecda021902d2d6ac12a1
SAVIN P <49348281+savinpadencherry@users.noreply.github.com>
2026-06-18 01:18:29 +0530

    refactor: adopt FHIR-style repository and bloc bootstrap

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
-import 'app/app.dart';
+import 'core/blocs/auth/auth_bloc.dart';
+import 'core/blocs/capture/capture_bloc.dart';
+import 'core/constants.dart';
+import 'core/get.dart';
+import 'core/repository/auth_repository.dart';
+import 'core/repository/capture_repository.dart';
+import 'core/services/navigator_service.dart';
+import 'theme/theme.dart';
+import 'views/home/home_view.dart';
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
-  runApp(const MrRexApp());
+  initializeGetIt();
+  runApp(const MainApplication());
+}
+
+class MainApplication extends StatelessWidget {
+  const MainApplication({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MultiRepositoryProvider(
+      providers: <RepositoryProvider<dynamic>>[
+        RepositoryProvider<AuthRepository>(
+          create: (_) => AuthRepository(),
+        ),
+        RepositoryProvider<CaptureRepository>(
+          create: (_) => CaptureRepository(),
+        ),
+      ],
+      child: MultiBlocProvider(
+        providers: <BlocProvider<dynamic>>[
+          BlocProvider<AuthBloc>(
+            create: (BuildContext context) => AuthBloc(
+              authRepository: context.read<AuthRepository>(),
+            )..add(const AuthStarted()),
+          ),
+          BlocProvider<CaptureBloc>(
+            create: (BuildContext context) => CaptureBloc(
+              captureRepository: context.read<CaptureRepository>(),
+            )..add(const CaptureRequested()),
+          ),
+        ],
+        child: MaterialApp(
+          title: ConstantVars.appName,
+          debugShowCheckedModeBanner: false,
+          navigatorKey: app<NavigatorService>().navigatorKey,
+          theme: AppTheme.light,
+          home: const HomeView(),
+        ),
+      ),
+    );
+  }
 }
