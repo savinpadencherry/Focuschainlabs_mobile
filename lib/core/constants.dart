84cce1bb6e43412bfc058ce6b544d47d13852296
SAVIN P <49348281+savinpadencherry@users.noreply.github.com>
2026-06-18 01:16:08 +0530

    refactor: add core constants

---
+++ lib/core/constants.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+abstract final class ConstantVars {
+  static const String appName = 'Mr. Rex';
+  static const String companyName = 'FocusChain Labs';
+  static const Color mainTheme = Color(0xFF146B63);
+  static const Color cardColorTheme = Color(0xFFE8F3F1);
+}
